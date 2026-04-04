36dc27ad23a3eefff45c3d11777837e21a0ce84f
Tan Wee Joe <84664178+w3joe@users.noreply.github.com>
2026-04-04 12:37:08 +0100

    Update next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -3,6 +3,21 @@ const createNextIntlPlugin = require('next-intl/plugin');
 const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  redirects: async () => {
+    return [
+      {
+        source: '/compare',
+        destination: '/result',
+        permanent: true
+      },
+      {
+        source: '/compare/:path*',
+        destination: '/result',
+        permanent: true
+      }
+    ];
+  }
+};
 
 module.exports = withNextIntl(nextConfig);
